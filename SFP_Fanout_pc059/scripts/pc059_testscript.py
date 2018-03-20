20b4d135f4acb1f7a837bb1300b2af7830af71d3
Paolo Baesso <paolo.baesso@bristol.ac.uk>
2018-03-20 14:31:19 +0000

    So far not much has changed

--- SFP_Fanout_pc059/scripts/pc059_testscript.py
+++ SFP_Fanout_pc059/scripts/pc059_testscript.py
@@ -43,7 +43,6 @@ if (enableCore):
    nwords= 1
    master_I2C.write(myslave, mycmd, mystop)
 
-
    mystop=False
    mycmd= [0x01]
    master_I2C.write(myslave, mycmd, mystop)
@@ -52,64 +51,94 @@ if (enableCore):
 
 #######################################################
 #EEPROM BEGIN
-zeEEPROM= E24AA025E48T(master_I2C, 0x57)
-res=zeEEPROM.readEEPROM(0xfa, 6)
-result="  EEPROM ID:\n\t"
-for iaddr in res:
-    result+="%02x "%(iaddr)
-print result
+doEeprom= True
+if doEeprom:
+  zeEEPROM= E24AA025E48T(master_I2C, 0x57)
+  res=zeEEPROM.readEEPROM(0xfa, 6)
+  result="  EEPROM ID:\n\t"
+  for iaddr in res:
+      result+="%02x "%(iaddr)
+  print result
 #EEPROM END
 
 #######################################################
-# #I2C EXPANDER CONFIGURATION BEGIN
-# IC28 EQUALIZER EXPANDER
-#IC28=PCA9539PW(master_I2C, 0x74)
-#BANK 0
-#IC28.setInvertReg(0, 0x00)# 0= normal
-#IC28.setIOReg(0, 0x00)# 0= output <<<<<<<<<<<<<<<<<<<
-#IC28.setOutputs(0, 0xFF)
-#res= IC28.getInputs(0)
-#print "IC28 read back bank 0: 0x%X" % res[0]
-#BANK 1
-#IC28.setInvertReg(1, 0x00)# 0= normal
-#IC28.setIOReg(1, 0x00)# 0= output <<<<<<<<<<<<<<<<<<<
-#IC28.setOutputs(1, 0xFF)
-#res= IC28.getInputs(1)
-#print "IC28 read back bank 1: 0x%X" % res[0]
-
-#######################################################
 #CLOCK CONFIGURATION BEGIN
-zeClock=si5345(master_I2C, 0x68)
-res= zeClock.getDeviceVersion()
-zeClock.checkDesignID()
-#zeClock.setPage(0, True)
-#zeClock.getPage(True)
-clkRegList= zeClock.parse_clk("./../../bitFiles/pc059_Si5345.txt")
-zeClock.writeConfiguration(clkRegList)######
-zeClock.writeRegister(0x0536, [0x0B]) #Configures manual switch of inputs
-zeClock.writeRegister(0x0949, [0x0F]) #Enable all inputs
-zeClock.writeRegister(0x052A, [0x05]) #Configures source of input
-iopower= zeClock.readRegister(0x0949, 1)
-print "  Clock IO power (REG 0x0949): 0x%X" % iopower[0]
-lol= zeClock.readRegister(0x000E, 1)
-print "  Clock LOL (REG 0x000E): 0x%X" % lol[0]
-los= zeClock.readRegister(0x000D, 1)
-print "  Clock LOS (REG 0x000D): 0x%X" % los[0]
+doClock = False
+if doClock:
+  zeClock=si5345(master_I2C, 0x68)
+  res= zeClock.getDeviceVersion()
+  zeClock.checkDesignID()
+  #zeClock.setPage(0, True)
+  #zeClock.getPage(True)
+  clkRegList= zeClock.parse_clk("./../../bitFiles/pc059_Si5345.txt")
+  zeClock.writeConfiguration(clkRegList)######
+  zeClock.writeRegister(0x0536, [0x0B]) #Configures manual switch of inputs
+  zeClock.writeRegister(0x0949, [0x0F]) #Enable all inputs
+  zeClock.writeRegister(0x052A, [0x05]) #Configures source of input
+  iopower= zeClock.readRegister(0x0949, 1)
+  print "  Clock IO power (REG 0x0949): 0x%X" % iopower[0]
+  lol= zeClock.readRegister(0x000E, 1)
+  print "  Clock LOL (REG 0x000E): 0x%X" % lol[0]
+  los= zeClock.readRegister(0x000D, 1)
+  print "  Clock LOS (REG 0x000D): 0x%X" % los[0]
 #CLOCK CONFIGURATION END
 
 #######################################################
 # #I2C EXPANDER CONFIGURATION BEGIN
 # IC28 EQUALIZER EXPANDER
-IC28=PCA9539PW(master_I2C, 0x74)
-#BANK 0
-IC28.setInvertReg(0, 0x00)# 0= normal
-IC28.setIOReg(0, 0x00)# 0= output <<<<<<<<<<<<<<<<<<<
-IC28.setOutputs(0, 0xFF)
-res= IC28.getInputs(0)
-print "IC28 read back bank 0: 0x%X" % res[0]
-#BANK 1
-IC28.setInvertReg(1, 0x00)# 0= normal
-IC28.setIOReg(1, 0x00)# 0= output <<<<<<<<<<<<<<<<<<<
-IC28.setOutputs(1, 0xFF)
-res= IC28.getInputs(1)
-print "IC28 read back bank 1: 0x%X" % res[0]
+doIC28= True
+if doIC28:
+  IC28=PCA9539PW(master_I2C, 0x74)
+  #BANK 0
+  IC28.setInvertReg(0, 0x00)# 0= normal
+  IC28.setIOReg(0, 0x00)# 0= output <<<<<<<<<<<<<<<<<<<
+  IC28.setOutputs(0, 0xFF)
+  res= IC28.getInputs(0)
+  print "IC28 read back bank 0: 0x%X" % res[0]
+  #BANK 1
+  IC28.setInvertReg(1, 0x00)# 0= normal
+  IC28.setIOReg(1, 0x00)# 0= output <<<<<<<<<<<<<<<<<<<
+  IC28.setOutputs(1, 0xFF)
+  res= IC28.getInputs(1)
+  print "IC28 read back bank 1: 0x%X" % res[0]
+# #I2C EXPANDER CONFIGURATION END
+
+#######################################################
+# #I2C EXPANDER CONFIGURATION BEGIN
+# IC29 SFP SIGNALS EXPANDER
+doIC29= True
+if doIC29:
+  IC29=PCA9539PW(master_I2C, 0x75)
+  #BANK 0
+  IC29.setInvertReg(0, 0x00)# 0= normal
+  IC29.setIOReg(0, 0x00)# 0= output <<<<<<<<<<<<<<<<<<<
+  IC29.setOutputs(0, 0xFF)
+  res= IC29.getInputs(0)
+  print "IC29 read back bank 0: 0x%X" % res[0]
+  #BANK 1
+  IC29.setInvertReg(1, 0x00)# 0= normal
+  IC29.setIOReg(1, 0xFF)# FF= input <<<<<<<<<<<<<<<<<<<
+  IC29.setOutputs(1, 0xFF)
+  res= IC29.getInputs(1)
+  print "IC29 read back bank 1: 0x%X" % res[0]
+# #I2C EXPANDER CONFIGURATION END
+
+#######################################################
+# #I2C EXPANDER CONFIGURATION BEGIN
+# IC27 MISC AND LED EXPANDER
+doIC27= True
+if doIC27:
+  IC27=PCA9539PW(master_I2C, 0x76)
+  #BANK 0
+  IC27.setInvertReg(0, 0x00)# 0= normal
+  IC27.setIOReg(0, 0x00)# 0= output (LED) <<<<<<<<<<<<<<<<<<<
+  IC27.setOutputs(0, 0xAA)
+  res= IC27.getInputs(0)
+  print "IC27 read back bank 0: 0x%X" % res[0]
+  #BANK 1
+  IC27.setInvertReg(1, 0x00)# 0= normal
+  IC27.setIOReg(1, 0x00)# 0= output <<<<<<<<<<<<<<<<<<<
+  IC27.setOutputs(1, 0xFF)
+  res= IC27.getInputs(1)
+  print "IC27 read back bank 1: 0x%X" % res[0]
+# #I2C EXPANDER CONFIGURATION END
